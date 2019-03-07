076b6ed7222b0a124fb69b2de33f9d1aa832f075
Henrik Hautakoski <henrik@eossweden.org>
2019-03-07 15:22:19 +0100

    cleos: adding helpers module.

---
+++ programs/cleos/helpers.cpp
@@ -0,0 +1,26 @@
+
+#include <cctype>
+#include "helpers.hpp"
+
+namespace eosio { namespace client { namespace helpers {
+
+	std::string& url_encode(std::string& str) {
+
+		char hex[17] = "0123456789ABCDEF";
+
+		for(int i = 0; i < str.length(); i++) {
+
+			char ch = str[i];
+
+			if (!(isalnum(ch) || ch == '-' || ch == '_' || ch == '.' || ch == '~') ) {
+				std::string rep = "%";
+				rep += hex[(ch & 0xF0) >> 4];
+				rep += hex[ch & 0x0F];
+				str.replace(i, 1, rep);
+				i += 2;
+			}
+		}
+        return str;
+    }
+
+}}}
---
+++ programs/cleos/helpers.hpp
@@ -0,0 +1,13 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE
+ */
+#pragma once
+
+#include <string>
+
+namespace eosio { namespace client { namespace helpers {
+
+	std::string& url_encode(std::string& str);
+
+}}}
