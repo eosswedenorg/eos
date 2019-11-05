a544fef9656965ba7dc2ffad5b2238d8e6e3f06b
Henrik Hautakoski <henrik@eossweden.org>
2019-11-05 11:38:30 +0100

    CMakeModules/package.cmake: update info for cleos-v2

--- CMakeModules/package.cmake
+++ CMakeModules/package.cmake
@@ -1,8 +1,8 @@
-set(VENDOR "block.one")
-set(PROJECT_NAME "eosio")
+set(VENDOR "EOS Sw/eden")
+set(PROJECT_NAME "cleos-v2")
 set(DESC "Software for the EOS.IO network")
-set(URL "https://github.com/eosio/eos")
-set(EMAIL "support@block.one")
+set(URL "https://github.com/EOS-Mainnet/eos")
+set(EMAIL "https://github.com/EOS-Mainnet/eos/issues")
 
 configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_package.sh.in ${CMAKE_BINARY_DIR}/packages/generate_package.sh @ONLY)
 configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_bottle.sh ${CMAKE_BINARY_DIR}/packages/generate_bottle.sh COPYONLY)
